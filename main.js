3f0097b73305ad61ed8fed59771e6dd1c1544e87
Clement Sauvage <clement.sauvage@hei.yncrea.fr>
2020-10-23 17:23:01 +0200

    Add match function

--- main.js
+++ main.js
@@ -1,6 +1,27 @@
+/**
+ * Returns true if the pattern is the same as text, else return false
+ * @param {*} pattern 
+ * @param {*} text 
+ * @returns {boolean}
+ */
 function matchOne(pattern, text) {
     if (!pattern) return true; // Any text matches an empty pattern
     if (!text) return false; // If the pattern is defined but the text is empty, there cannot be a match
     if (pattern === ".") return true; // Any inputted text matches the wildcard
     return pattern === text;
   }
+
+/**
+ * Returns true if the pattern is met in text, else return false
+ * @param {*} pattern 
+ * @param {*} text 
+ * @returns {boolean}
+ */
+function match(pattern, text) {
+if (pattern === "") return true;
+// Our base case - if the pattern is empty, any inputted text is a match
+else
+    return (
+    matchOne(pattern[0], text[0]) && match(pattern.slice(1), text.slice(1))
+    );
+}
